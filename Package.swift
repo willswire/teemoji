cae062780c7e81805166b9bbd28a0674f39710d2
William Walker <w_walker@icloud.com>
2025-01-26 17:07:38 -0600

    feat: add support for older versions of macOS

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "teemoji",
     platforms: [
-        .macOS(.v15)
+        .macOS(.v11)
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
