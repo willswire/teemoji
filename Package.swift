36a7778d7639b915162329a06490f3f6fa8f5907
William Walker <w_walker@icloud.com>
2025-01-26 18:01:23 -0600

    feat: add basic testing

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version: 5.10.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -9,10 +8,13 @@ let package = Package(
         .macOS(.v11)
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "teemoji",
-            resources: [.copy("TeemojiClassifier.mlmodelc")])
+            resources: [.copy("TeemojiClassifier.mlmodelc")]
+        ),
+        .testTarget(
+            name: "TeemojiTests",
+            dependencies: ["teemoji"]
+        ),
     ]
 )
